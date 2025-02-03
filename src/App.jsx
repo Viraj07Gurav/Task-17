cb3d9048bfb72049ab3e2b4abec8374fa97fa0c2
viraj_gurav <viraj07gurav@gmail.com>
2025-02-03 18:21:15 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,46 @@ import LiveHealth from './Component/LiveHealth'
 import MainContent from './Component/MainContent'
 
 function App() {
- 
-
   return (
-    <div className="relative">
-      <div class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 pt-5 pb-24 border-2">
-      <div class="w-full max-w-md min-h-screen border-2">
-      <MainContent/>
-       <LiveHealth/>
-        <BottomNavigation/>
-        
+    <div className="flex flex-col gap-[2rem]">
+      <div className="flex items-center justify-center min-h-screen border-2">
+        <div className="w-full max-w-md min-h-screen relative flex flex-col">
+          <MainContent />
+          <LiveHealth />
+
+          {/* Chatbot Button inside container */}
+          <div className="relative w-full">
+            <button
+              type="button"
+              className="z-20 text-white flex flex-col  justify-around 
+              fixed bottom-25 right-5 rounded-lg"
+            >
+              <div className="p-3 rounded-full border-4 border-white bg-green-600">
+                <svg
+                  className="w-6 h-6 lg:w-6 lg:h-6 xl:w-10 xl:h-10"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </button>
+          </div>
+
+          {/* Bottom Navigation inside container */}
+          <div className="sticky bottom-0 left-0 right-0">
+            <BottomNavigation />
+          </div>
+
         </div>
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -8,19 +8,19 @@ function Header() {
         <div>
             <div>
                 <div className="h-10 p-3 bg-white shadow-lg rounded-2xl mb-5 h-15">
-                    <div className='flex justify-between'>
+                    <div className='flex justify-between '>
                         <div>
                             <span className='font-extrabold text-xl'>Fleet Edge</span>
                         </div>
-                        <div className='flex'>
+                        <div className='flex gap-2'>
                             <div>
-                            <FontAwesomeIcon icon={faTriangleExclamation} className="text-xl text-gray-800" />
+                            <FontAwesomeIcon icon={faTriangleExclamation} className=" text-lg text-gray-500" />
                             </div>
                             <div>
-                            <FontAwesomeIcon icon={faLightbulb} className="text-xl text-gray-800 hover:text-gray-700" />
+                            <FontAwesomeIcon icon={faLightbulb} className="text-lg text-gray-500 hover:text-gray-500" />
                             
                             </div>
-                            <div><svg className="w-6 h-6 text-gray-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <div><svg className="w-6 h-6 text-gray-500 dark:text-[gray]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5.365V3m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175 0 .593 0 1.292-.538 1.292H5.538C5 18 5 17.301 5 16.708c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 12 5.365ZM8.733 18c.094.852.306 1.54.944 2.112a3.48 3.48 0 0 0 4.646 0c.638-.572 1.236-1.26 1.33-2.112h-6.92Z" />
                             </svg>
                             </div>
